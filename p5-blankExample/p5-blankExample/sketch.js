2bd93e2aaf1f19d99da9def91fdf5562285b7ae1
GitMouHub <43281668+GitMouHub@users.noreply.github.com>
2018-10-12 14:41:45 -0700

    in-class exercise

--- p5-blankExample/p5-blankExample/sketch.js
+++ p5-blankExample/p5-blankExample/sketch.js
@@ -18,17 +18,11 @@ console.log("there are " + birds + "birds")
   // set background color in grayscale:
   background(150, 0, 150);
 
-  // drawing a green ellipse at the right of the screen
-  fill(random(0, 255));
-  strokeWeight(0);
-  ellipse(400, 200, 200, 80);
-
   // draw rectangle
-  fill(0, 100, 100);
-  stroke(255, 0, 0);
+  fill(255, 100, 50, 127);
+  stroke(100, 200, mouseX, mouseY);
   strokeWeight(10);
-  rectMode(CENTER)
-  rect(mouseX, mouseY, 80, 150)
+  rect(mouseX, mouseY, 38, 31, 86, 20, 69, 63, 30, 76)
 
   console.log("mouse X is" + mouseX);
 }
